453b8951b9f98d39367188eab2ac06d2d13f08a8
jalpert <jalpert911@gmail.com>
2018-06-26 10:48:17 +0300

    Removed bias

--- assignment1/cs231n/classifiers/neural_net.py
+++ assignment1/cs231n/classifiers/neural_net.py
@@ -109,7 +109,7 @@ class TwoLayerNet(object):
         loss_i = -scores[i][y[i]] + np.log(np.sum(np.exp(scores[i])))
         loss = loss + loss_i / N'''
 
-    f = scores - np.matrix(np.max(scores, axis=1)).T
+    f = scores# - np.matrix(np.max(scores, axis=1)).T
     term1 = -f[np.arange(N), y]
     sum_j = np.sum(np.exp(f), axis=1)
     term2 = np.log(sum_j)
